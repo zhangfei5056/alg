caca2bad690aeefaa463c171e92adc6f1f43443a
Yuan Cao <yuan.cao@agoda.com>
2017-10-16 14:23:59 +0700

    init

---
+++ ALG/ALG/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ALG
+//
+//  Created by Cao, Yuan (Agoda) on 10/16/17.
+//  Copyright © 2017 feizhang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
